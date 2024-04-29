17fb3a6245b55a1fd80ad558afe47c2a23ba4783
11D-Beyonder <ztz20001117@126.com>
2024-04-29 17:06:54 +0800

    venv-selector优化：只搜索conda的环境

--- lua/plugins/venv-selector.lua
+++ lua/plugins/venv-selector.lua
@@ -3,13 +3,15 @@ return {
     "linux-cultist/venv-selector.nvim",
     dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
     opts = {
+      search_workspace = false,
+      search = false,
       anaconda_base_path = (vim.loop.os_uname().sysname == "Windows_NT") and "~/AppData/Local/Programs/miniconda3"
         or "~/miniconda3",
       anaconda_envs_path = (vim.loop.os_uname().sysname == "Windows_NT") and "~/AppData/Local/Programs/miniconda3/envs"
         or "~/miniconda3/envs",
       anaconda = {
         python_executable = "python",
-        python_parent_dir = "",
+        python_parent_dir = (vim.loop.os_uname().sysname == "Windows_NT") and "" or "bin",
       },
     },
     keys = {
