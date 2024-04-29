1dc5060e73e5be80bea988b846d377d1b8d363bd
11D-Beyonder <ztz20001117@126.com>
2024-04-29 13:32:23 +0800

    DAP快捷键初步设置

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,5 +1,35 @@
 return {
   {
+    "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require "dap"
+      dap.adapters.python = {
+        type = "executable",
+        command = "python",
+        args = { "-m", "debugpy.adapter" },
+      }
+      dap.configurations.python = {
+        {
+          type = "python",
+          request = "launch",
+          name = "Launch file",
+          program = "${file}",
+          pythonPath = function()
+            return "python"
+          end,
+        },
+      }
+    end,
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio",
+    },
+    config = true,
+  },
+  {
     "jay-babu/mason-nvim-dap.nvim",
     dependencies = {
       "williamboman/mason.nvim",
