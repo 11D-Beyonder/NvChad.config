24945980da58b0b22330dab8862738aa6ba97713
11D-Beyonder <ztz20001117@126.com>
2024-05-02 00:32:56 +0800

    编辑器开启终端的快捷键设置

--- lua/mappings/terminal.lua
+++ lua/mappings/terminal.lua
@@ -1,21 +1,21 @@
 local map = vim.keymap.set
-map("t", "<C-x>", "<C-\\><C-N>", { desc = "terminal escape terminal mode" })
-map("n", "<leader>h", function()
+map("t", "<esc>", "<C-\\><C-N>", { desc = "escape terminal mode" })
+map("n", "<leader>th", function()
   require("nvchad.term").new { pos = "sp" }
-end, { desc = "terminal new horizontal term" })
-map("n", "<leader>v", function()
+end, { desc = "new horizontal term" })
+map("n", "<leader>tv", function()
   require("nvchad.term").new { pos = "vsp" }
-end, { desc = "terminal new vertical window" })
+end, { desc = "new vertical window" })
 map({ "n", "t" }, "<A-v>", function()
   require("nvchad.term").toggle { pos = "vsp", id = "vtoggleTerm" }
-end, { desc = "terminal toggleable vertical term" })
+end, { desc = "toggleable vertical term" })
 map({ "n", "t" }, "<A-h>", function()
   require("nvchad.term").toggle { pos = "sp", id = "htoggleTerm" }
-end, { desc = "terminal new horizontal term" })
-map({ "n", "t" }, "<A-i>", function()
+end, { desc = "toggle horizontal term" })
+map({ "n", "t" }, "<A-f>", function()
   require("nvchad.term").toggle { pos = "float", id = "floatTerm" }
-end, { desc = "terminal toggle floating term" })
-map("t", "<ESC>", function()
+end, { desc = "toggle floating term" })
+map("t", "<C-q>", function()
   local win = vim.api.nvim_get_current_win()
   vim.api.nvim_win_close(win, true)
-end, { desc = "terminal close term in terminal mode" })
+end, { desc = "close term in terminal mode" })
