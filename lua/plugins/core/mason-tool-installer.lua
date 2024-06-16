880236c7560338770c5ed592c28ed584b8ffc407
11D-Beyonder <ztz20001117@gmail.com>
2024-06-16 22:36:12 +0800

    支持Java调试

--- lua/plugins/core/mason-tool-installer.lua
+++ lua/plugins/core/mason-tool-installer.lua
@@ -1,25 +1,26 @@
 return {
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    opts = {
-      ensure_installed = {
-        -- LSP
-        "lua-language-server",
-        "rust-analyzer",
-        "pyright",
-        "taplo",
-        "json-lsp",
-        "jdtls",
-        "lemminx",
-        "clangd",
-        -- Formatter
-        "isort",
-        "black",
-        -- DAP
-        "debugpy",
-        "codelldb",
-        "java-debug-adapter",
-      },
-    },
-  },
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		opts = {
+			ensure_installed = {
+				-- LSP
+				"lua-language-server",
+				"rust-analyzer",
+				"pyright",
+				"taplo",
+				"json-lsp",
+				"jdtls",
+				"lemminx",
+				"clangd",
+				-- Formatter
+				"isort",
+				"black",
+				-- DAP
+				"debugpy",
+				"codelldb",
+				"java-debug-adapter",
+				"java-test",
+			},
+		},
+	},
 }
--- lua/plugins/langpacks/java.lua
+++ lua/plugins/langpacks/java.lua
@@ -2,24 +2,14 @@ return {
 	{
 		"mfussenegger/nvim-jdtls",
 		ft = { "java" },
+		dependencies = {
+			"mfussenegger/nvim-dap",
+		},
 		opts = function()
 			-- calculate workspace dir
 			local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
 			local workspace_dir = vim.fn.stdpath("data") .. "/site/java/workspace-root/" .. project_name
-			os.execute("mkdir " .. workspace_dir)
-
-			-- get the mason install path
-			local install_path = require("mason-registry").get_package("jdtls"):get_install_path()
-
-			-- get the current OS
-			local os
-			if vim.fn.has("mac") == 1 then
-				os = "mac"
-			elseif vim.fn.has("unix") == 1 then
-				os = "linux"
-			elseif vim.fn.has("win32") == 1 then
-				os = "win"
-			end
+			vim.fn.mkdir(workspace_dir, "p")
 
 			return {
 				cmd = {
@@ -29,7 +19,7 @@ return {
 					"-Declipse.product=org.eclipse.jdt.ls.core.product",
 					"-Dlog.protocol=true",
 					"-Dlog.level=ALL",
-					"-javaagent:" .. install_path .. "/lombok.jar",
+					"-javaagent:" .. vim.fn.expand("$MASON/share/jdtls/lombok.jar"),
 					"-Xms1g",
 					"--add-modules=ALL-SYSTEM",
 					"--add-opens",
@@ -37,23 +27,66 @@ return {
 					"--add-opens",
 					"java.base/java.lang=ALL-UNNAMED",
 					"-jar",
-					vim.fn.glob(install_path .. "/plugins/org.eclipse.equinox.launcher_*.jar"),
+					vim.fn.expand("$MASON/share/jdtls/plugins/org.eclipse.equinox.launcher.jar"),
 					"-configuration",
-					install_path .. "/config_" .. os,
+					vim.fn.expand("$MASON/share/jdtls/config"),
 					"-data",
 					workspace_dir,
 				},
 				root_dir = vim.fs.root(0, { ".git", "mvnw", "gradlew" }),
 				settings = {
-					java = {},
+					java = {
+						eclipse = { downloadSources = true },
+						configuration = { updateBuildConfiguration = "interactive" },
+						maven = { downloadSources = true },
+						implementationsCodeLens = { enabled = true },
+						referencesCodeLens = { enabled = true },
+						inlayHints = { parameterNames = { enabled = "all" } },
+						signatureHelp = { enabled = true },
+						completion = {
+							favoriteStaticMembers = {
+								"org.hamcrest.MatcherAssert.assertThat",
+								"org.hamcrest.Matchers.*",
+								"org.hamcrest.CoreMatchers.*",
+								"org.junit.jupiter.api.Assertions.*",
+								"java.util.Objects.requireNonNull",
+								"java.util.Objects.requireNonNullElse",
+								"org.mockito.Mockito.*",
+							},
+						},
+						sources = {
+							organizeImports = {
+								starThreshold = 9999,
+								staticStarThreshold = 9999,
+							},
+						},
+					},
 				},
 				init_options = {
-					bundles = {},
+					bundles = {
+						vim.fn.expand("$MASON/share/java-debug-adapter/com.microsoft.java.debug.plugin.jar"),
+					},
 				},
+				filetypes = { "java" },
 			}
 		end,
 		config = function(_, opts)
-			require("jdtls").start_or_attach(opts)
+			vim.api.nvim_create_autocmd("Filetype", {
+				pattern = "java", -- autocmd to start jdtls
+				callback = function()
+					require("jdtls").start_or_attach(opts)
+				end,
+			})
+			vim.api.nvim_create_autocmd("LspAttach", {
+				pattern = "*.java",
+				callback = function(args)
+					local client = vim.lsp.get_client_by_id(args.data.client_id)
+					-- ensure that only the jdtls client is activated
+					if client ~= nil and client.name == "jdtls" then
+						require("jdtls.dap").setup_dap_main_class_configs()
+					end
+				end,
+			})
 		end,
 	},
 }
